29b2e33808230872931335ef9b3af53dd869e9ac
kkhhss <yg91238405@gmail.com>
2023-08-17 20:06:10 +0900

    TabHost 직접 풀어보기 미완성

--- app/src/main/java/kr/hs/emirim/kkhhss/taphosttest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/kkhhss/taphosttest/MainActivity.kt
@@ -13,16 +13,20 @@ class MainActivity : TabActivity() {
         var tabHost = this.tabHost
 
         var tabSpec1 = tabHost.newTabSpec("spec1").setIndicator("아이브")
-        tabSpec1.setContent(R.id.linear1)
+        tabSpec1.setContent(R.id.img1)
         tabHost.addTab(tabSpec1)
 
         var tabSpec2 = tabHost.newTabSpec("spec2").setIndicator("뉴진스")
-        tabSpec2.setContent(R.id.linear2)
+        tabSpec2.setContent(R.id.img2)
         tabHost.addTab(tabSpec2)
 
         var tabSpec3 = tabHost.newTabSpec("spec3").setIndicator("트와이스")
-        tabSpec3.setContent(R.id.linear3)
+        tabSpec3.setContent(R.id.img3)
         tabHost.addTab(tabSpec3)
+
+        var tabSpec4 = tabHost.newTabSpec("spec4").setIndicator("아이들")
+        tabSpec4.setContent(R.id.img4)
+        tabHost.addTab(tabSpec4)
 
         tabHost.currentTab = 1
     }
